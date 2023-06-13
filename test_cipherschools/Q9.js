a606f1104742cc80756c7c6a4196eb29282a6f0d
Faiz <faizalam4e@gmail.com>
2023-06-13 09:16:37 +0530

    new commit

---
+++ test_cipherschools/Q9.js
@@ -0,0 +1,6 @@
+// Question: Create a Node.js program that appends the text "Hello content!" to a file called "mynewfile1.txt" using the fs.appendFile() method.
+const fs=require('fs');
+fs.appendFile("./mynewfile1.txt","Hello content!",(err)=>{
+    if(err){console.log(err);}
+    console.log("Appended successfully")
+});
